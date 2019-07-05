a87efe6c15baebc797cf3567c7281ac42db76eb1
dayubadel <dayana_bailon@outlook.com>
2019-07-05 16:36:32 -0500

    voto unico

--- src/app/participants/participants.page.ts
+++ src/app/participants/participants.page.ts
@@ -3,6 +3,8 @@ import { Storage} from '@ionic/storage';
 import { ParticipantsService } from '../../providers/participants.service';
 import { ActivatedRoute}  from '@angular/router'
 import{ urlContent } from '../../environments/environment';
+import { AlertController } from '@ionic/angular';
+import { VotoService } from '../../providers/voto.service';
 
 @Component({
   selector: 'app-participants',
@@ -16,17 +18,22 @@ export class ParticipantsPage implements OnInit {
   public _participantes : any [];
   public _codigoEvento : string;
   public _urlContent : string;
+  public _validarEspecialista : boolean=false;
+  public _idAsignarTipoUsuarioEncriptado : string ="";
 
   constructor(
     private storage: Storage,
     private participantService: ParticipantsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private alertController: AlertController,
+    private voteService : VotoService
   ) { }
 
   ngOnInit() {
     this.storage.get('idAsignarTipoUsuario').then(
       (valAsignarTipoUsuario) => 
       {
+        this._idAsignarTipoUsuarioEncriptado = valAsignarTipoUsuario;
         this.storage.get('idAsignarCategoriaConfigurarEvento').then((valAsignarCategoriaConfigurarEvento) => 
         {
             this.cargarParticipantes(valAsignarTipoUsuario,valAsignarCategoriaConfigurarEvento);
@@ -47,8 +54,44 @@ export class ParticipantsPage implements OnInit {
         if(data['_validar']==true)
         {
           this._participantes = data['_objeto'];
-          console.log(data);
+          this._validarEspecialista = data['_validarEspecialista'];
         }
+      });
+  }
+
+  async mensajeVotoUnico(idConfigurarTipoActorEvaluadoEncriptado : string) {
+    const alert = await this.alertController.create({
+      header: 'Importante!',
+      message: 'Solo se puede <strong>votar</strong> por un stand en esta categoría!!!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {            
+          }
+        }, {
+          text: 'Votar',
+          handler: () => {
+             this.votarVotoUnico(idConfigurarTipoActorEvaluadoEncriptado);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+  votarVotoUnico(idConfigurarTipoActorEvaluadoEncriptado: string)
+  {
+    this.voteService.postSingleVote(this._idAsignarTipoUsuarioEncriptado,idConfigurarTipoActorEvaluadoEncriptado).then(data =>
+      {
+       if(data['_validar']==true){
+          console.log(data);
+       }       
+        this._validar = data['_validar'];
+        this._mensaje=data['_mensaje'];   
       });
   }
 
